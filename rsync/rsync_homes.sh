e6915d834eefe87050602f3a64ae15cdb3edd395
ziyuan <hanziyuan08@outlook.com>
2020-12-30 19:04:43 +0800

    Add log

--- rsync/rsync_homes.sh
+++ rsync/rsync_homes.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-rsync -av --delete /share/CACHEDEV1_DATA/homes /share/CACHEDEV2_DATA/Backups
+rsync -av --delete /share/CACHEDEV1_DATA/homes /share/CACHEDEV2_DATA/Backups >> /share/my_cron/rsync/rsync_homes_disk2.log
+
+# ssh ziyuan@192.168.31.21 << EOF
+#     cd ~/wol
+#     echo 123
+# EOF
